7537a94d787520465bdb924fcb7a319bd14d79e8
zebercet0 <alierenesen@gmail.com>
2022-10-20 11:25:03 -0400

    kind of a snapshot // trying to implement passport

---
+++ models/User.js
@@ -0,0 +1,10 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+	username: {type: String, required: true},
+	password: {type: String, required: true}
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require("passport-local");
 const _ = require('lodash');
+const User = require(__dirname + '/models/User'); 
 
 const app = express();
-
-
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
 
 //mongodb bağlantısı
 const dbURI = 'mongodb+srv://alieren:alieren@node-db.rkxyvry.mongodb.net/?retryWrites=true&w=majority';
@@ -19,7 +22,26 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get('/', (req, res) =>{
 	res.render(__dirname + '/views/index.ejs');
 });
+
+app.get('/login', (req, res) =>{
+	res.render(__dirname + '/views/login.ejs');
+});
+
+
+
+app.get('/register', (req, res) =>{
+	res.render(__dirname + '/views/register.ejs');
+});
+
+
